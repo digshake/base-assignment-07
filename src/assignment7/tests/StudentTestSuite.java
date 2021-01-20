c439ddb4928bf8300fafdd7ce4a5c81f05c79ebe
Dennis Cosgrove <cosgroved@wustl.edu>
2021-01-20 14:04:49 -0600

    initial commit after copy from wustl-cse131-sp20

---
+++ src/assignment7/tests/StudentTestSuite.java
@@ -0,0 +1,28 @@
+package assignment7.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import assignment7.tests.student.StudentBearBucksTest;
+import assignment7.tests.student.StudentFullNameTest;
+import assignment7.tests.student.StudentGradePointAverageEdgeCaseTest;
+import assignment7.tests.student.StudentGradePointAverageTest;
+import assignment7.tests.student.StudentIdTest;
+import assignment7.tests.student.StudentLegacyBearBucksTest;
+import assignment7.tests.student.StudentLegacyNameTest;
+import assignment7.tests.student.StudentPhiBetaKappaTest;
+import assignment7.tests.student.StudentStandingTest;
+import assignment7.tests.student.StudentToStringIsOverriddenTest;
+import assignment7.tests.student.StudentToStringTest;
+
+/**
+ * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ StudentFullNameTest.class, StudentIdTest.class,
+		StudentGradePointAverageTest.class,
+		StudentGradePointAverageEdgeCaseTest.class, StudentStandingTest.class, StudentPhiBetaKappaTest.class,
+		StudentBearBucksTest.class, StudentLegacyNameTest.class,
+		StudentLegacyBearBucksTest.class, StudentToStringIsOverriddenTest.class, StudentToStringTest.class })
+public class StudentTestSuite {
+}
